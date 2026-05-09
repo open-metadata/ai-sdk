435f60df756908ba7a72d7676ac8ce980d5db4fa
Pere Miquel Brull <peremiquelbrull@gmail.com>
2026-05-09 13:13:32 +0200

    feat(dbt): add days_between adapter dispatch macro

---
+++ cookbook/resources/demo-database/dbt/macros/days_between.sql
@@ -0,0 +1,12 @@
+{# Returns the number of whole days from start_date to end_date as integer. #}
+{% macro days_between(start_date, end_date) %}
+  {{ return(adapter.dispatch('days_between')(start_date, end_date)) }}
+{% endmacro %}
+
+{% macro default__days_between(start_date, end_date) %}
+  ({{ end_date }} - {{ start_date }})
+{% endmacro %}
+
+{% macro trino__days_between(start_date, end_date) %}
+  date_diff('day', {{ start_date }}, {{ end_date }})
+{% endmacro %}
